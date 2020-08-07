6100796f1351d624a182ff189be301b2925cd627
andrewhpark25 <andrewhpark25@gmail.com>
2020-08-07 14:21:49 -0700

    changes to indexRouter.js

--- human-rights-back-end/index.js
+++
@@ -1,7 +0,0 @@
-require('dotenv').config();
-const server = require("./api/server");
-
-const PORT = process.env.PORT || 8000;
-server.listen(PORT, () => {
-  console.log(`\n=== Server listening on port ${PORT} ===\n`);
-});
