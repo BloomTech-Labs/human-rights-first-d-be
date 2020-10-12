cf8682dfb1f29670bee94086e04a92436affc8a9
Virginia Davenport <virginialdport@gmail.com>
2020-10-12 09:46:51 -0600

    created endpoint that connects directly to ds api

--- api/dsService/dsRouter.js
+++ api/dsService/dsRouter.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const axios = require('axios');
 const router = express.Router();
 const dsModel = require('./dsModel');
 const authRequired = require('../middleware/authRequired');
@@ -23,14 +24,31 @@ router.get('/incidents', function (req, res) {
   dsModel
     .getData()
     .then((response) => {
-      axios.post('https://labs27-d-hrf-api.herokuapp.com/incidents/addIncidents', response.data)
+      const obj = JSON.parse(response.data);
+      console.log(obj);
+      axios.post(
+        // 'https://labs27-d-hrf-api.herokuapp.com/incidents/addIncidents',
+        'http://localhost:8000/incidents/addIncidents',
+        obj
+      );
     })
     .catch((error) => {
       console.log(error);
-      res.status(500).json({message: error, error_found: true});
-    })
+      res.status(500).json({ message: error, error_found: true });
+    });
 });
 
+router.get('/proxy', function (req, res) {
+  dsModel
+    .getData()
+    .then((response) => {
+      let info = JSON.parse(response.data);
+      res.status(200).json(info);
+    })
+    .catch((error) => {
+      res.status(500).json(error);
+    });
+});
 // router.get('/incidents/:id', function (req, res) {
 //   //gets specific incident
 // });
--- api/incidentsService/incidentsRouter.js
+++ api/incidentsService/incidentsRouter.js
@@ -1,15 +1,16 @@
-const express = require('express')
-const router = express.Router()
-const Incidents = require('./incidentsModel')
+const express = require('express');
+const router = express.Router();
+const Incidents = require('./incidentsModel');
 
 router.post('/addIncidents', (req, res) => {
-    const incidentsArray = req.body;
-    
-	Incidents.addIncidents(incidentsArray)
-		    .then((response) => {
-		     	res.status(201).json({error_found: false, message: response})
-             })
-             .catch((error) =>  {
-                 res.status(500).json({error_found: true, message: error})
-             })
-})
+  const incidentsArray = req.body;
+
+  Incidents.addIncidents(incidentsArray)
+    .then((response) => {
+      res.status(201).json({ error_found: false, message: response });
+    })
+    .catch((error) => {
+      res.status(500).json({ error_found: true, message: error });
+    });
+});
+module.exports = router;
