7b32b3dadba844f5cb409ea3a70104ba3c6c344f
Virginia Davenport <virginialdport@gmail.com>
2020-10-13 14:15:43 -0600

    fixed proxy endpoint, added findById, added README template

--- api/incidentsService/incidentsModel.js
+++ api/incidentsService/incidentsModel.js
@@ -2,8 +2,22 @@ const db = require('../../data/db-config');
 
 module.exports = {
   addIncidents,
+  findIncidentById,
 };
 
 function addIncidents(arr) {
   return db('incidents').insert(arr);
+}
+
+// async function addSources(obj){
+//     const[id] = await db('sources').insert(obj).returning("id")
+// }
+
+async function findIncidentById(id) {
+  const incident = await db('incidents').where({ id }).first();
+
+  const sources = await db('sources').where({ incident_id: id }).select('*');
+
+  const result = { ...incident, sources };
+  return result;
 }
--- data/migrations/20200625220949_create-profile.js
+++ data/migrations/20200625220949_create-profile.js
@@ -1,40 +1,49 @@
 exports.up = (knex) => {
-  return knex.schema
-    .raw('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"')
-    .createTable('profiles', (table) => {
-      table.string('id').notNullable().unique().primary();
-      table.string('email');
-      table.string('name');
-      table.string('avatarUrl');
-      table.timestamps(true, true);
-    })
-    .createTable('incidents', (table) => {
-      table.increments('table_id');
-      table.string('id').notNullable();
-      table.string('state');
-      table.string('city');
-    });
-  // .createTable('types', (table) => {
-  //   table.increments();
-  //   table.string('type').notNullable();
-  // })
-  // .createTable('saved_incidents', (table) => {
-  //   table.increments();
-  //   table
-  //     .string('incident_id')
-  //     .references('incidents.table_id')
-  //     .notNullable();
-  //   table.string('profile_id').references('profiles.id').notNullable();
-  // })
-  // .createTable('sources', (table) => {
-  //   table.increments();
-  //   table
-  //     .string('incident_id')
-  //     .references('incidents.table_id')
-  //     .notNullable();
-  //   table.string('link', 1000).notNullable();
-  //   table.string('origin', 255);
-  // })
+  return (
+    knex.schema
+      .raw('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"')
+      .createTable('profiles', (table) => {
+        table.string('id').notNullable().unique().primary();
+        table.string('email');
+        table.string('name');
+        table.string('avatarUrl');
+        table.timestamps(true, true);
+      })
+      .createTable('incidents', (table) => {
+        // table.string('table_id').notNullable().unique().primary();
+        table.string('id').notNullable().unique().primary();
+        table.string('state');
+        table.string('city');
+        // table.string('date').notNullable();
+        // table.string('title', 1000).notNullable();
+        // table.string('description');
+        table.float('lat').notNullable();
+        table.float('long').notNullable();
+        // table.boolean('lethal', 255);
+        // table.string('race_victim', 255);
+        // table.string('race_police_officer', 255);
+        // table.string('victim_gender', 255);
+        // table.string('police_officer_gender', 255);
+      })
+      // .createTable('types', (table) => {
+      //   table.increments();
+      //   table.string('type').notNullable();
+      // })
+      // .createTable('saved_incidents', (table) => {
+      //   table.increments();
+      //   table
+      //     .string('incident_id')
+      //     .references('incidents.table_id')
+      //     .notNullable();
+      //   table.string('profile_id').references('profiles.id').notNullable();
+      // })
+      .createTable('sources', (table) => {
+        table.increments();
+        table.string('incident_id').references('incidents.id').notNullable();
+        table.string('link', 1000).notNullable();
+        // table.string('origin', 255);
+      })
+  );
   // .createTable('incidents_type', (table) => {
   //   table.increments();
   //   table
@@ -48,7 +57,7 @@ exports.down = (knex) => {
   return (
     knex.schema
       // .dropTableIfExists('incidents_type')
-      // .dropTableIfExists('sources')
+      .dropTableIfExists('sources')
       // .dropTableIfExists('saved_incidents')
       // .dropTableIfExists('types')
       .dropTableIfExists('incidents')
