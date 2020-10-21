ee38d90a1b063cf8446f2ee88f9b9914d46b6909
Juan (programming) <programjuan@Juans-MacBook-Pro-2.local>
2020-10-21 12:41:47 -0400

    linting...

--- data/migrations/20200625220949_create-profile.js
+++ data/migrations/20200625220949_create-profile.js
@@ -5,39 +5,35 @@ types (id, type). Then an incident_type table, that has incident_id and type_id.
 */
 
 exports.up = (knex) => {
-  return (
-    knex.schema
-      .raw('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"')
-      .createTable('profiles', (table) => {
-        table.string('id').notNullable().unique().primary();
-        table.string('email');
-        table.string('name');
-        table.string('avatarUrl');
-        table.timestamps(true, true);
-      })
-      .createTable('incidents', (table) => {
-        table.string('id').notNullable().unique().primary();
-        table.string('state');
-        table.string('city');
-        table.string('date').notNullable();
-        table.string('title', 1000).notNullable();
-        table.string('description', 10000);
-        table.float('lat').notNullable();
-        table.float('long').notNullable();
-      })
-      .createTable('sources', (table) => {
-        table.increments();
-        table.string('incident_id').references('incidents.id').notNullable();
-        table.string('link', 1000).notNullable();
-      })
-  );
+  return knex.schema
+    .raw('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"')
+    .createTable('profiles', (table) => {
+      table.string('id').notNullable().unique().primary();
+      table.string('email');
+      table.string('name');
+      table.string('avatarUrl');
+      table.timestamps(true, true);
+    })
+    .createTable('incidents', (table) => {
+      table.string('id').notNullable().unique().primary();
+      table.string('state');
+      table.string('city');
+      table.string('date').notNullable();
+      table.string('title', 1000).notNullable();
+      table.string('description', 10000);
+      table.float('lat').notNullable();
+      table.float('long').notNullable();
+    })
+    .createTable('sources', (table) => {
+      table.increments();
+      table.string('incident_id').references('incidents.id').notNullable();
+      table.string('link', 1000).notNullable();
+    });
 };
 
 exports.down = (knex) => {
-  return (
-    knex.schema
-      .dropTableIfExists('sources')
-      .dropTableIfExists('incidents')
-      .dropTableIfExists('profiles')
-  );
+  return knex.schema
+    .dropTableIfExists('sources')
+    .dropTableIfExists('incidents')
+    .dropTableIfExists('profiles');
 };
