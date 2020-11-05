16b3f7b12cce73733e035a511b7e5a97ec39d226
royeradames <royeraadames@gmail.com>
2020-11-05 14:11:48 -0500

    Add 404 case for us_map

--- api/ds_server/ds_server_router.js
+++ api/ds_server/ds_server_router.js
@@ -8,7 +8,15 @@ router.post('/us_map', async (req, res, next) => {
     // req.body.end_date
     // req.body.sort_by
     //
-    console.log(`before axios `);
+    // check that the body has the require inputs
+    const isMissingInputs =
+      !req.body.start_date && !req.body.end_date && !req.body.sort_by;
+
+    if (isMissingInputs) {
+      res
+        .status(404)
+        .json({ error: 'Incorrect start date, end date or sort by' });
+    }
 
     let a_state_unemployment_rate = await axios.post(
       `http://hrf-ds16.eba-fmbjvhg4.us-east-1.elasticbeanstalk.com/us_map`,
@@ -19,7 +27,6 @@ router.post('/us_map', async (req, res, next) => {
       }
     );
 
-    console.log(`axios call => ${a_state_unemployment_rate}`);
     res.status(200).json({
       unemployment_rate: a_state_unemployment_rate.data,
     });
