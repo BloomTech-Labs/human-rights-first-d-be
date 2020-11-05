1c2ea17754abf1aa0577dfbee47c1f6f999aa1de
royeradames <royeraadames@gmail.com>
2020-11-05 16:35:15 -0500

    add passing test for the no existing us_demo_pie

--- api/ds_server/ds_server.test.js
+++ api/ds_server/ds_server.test.js
@@ -35,3 +35,14 @@ describe('/us_bar', () => {
     expect(res.status).toBe(200);
   })
 })
+
+describe('/us_demo_pie', () => {
+  it('200 passing date to client',  async () => {
+    const res = await request(server).post(`/ds_server/us_demo_pie`).send({
+      user_input: "FL"
+    })
+
+    expect(res.status).toBe(200)
+    expect(res.body).toEqual(expect.any(Object))
+  })
+})
