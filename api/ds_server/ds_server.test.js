b122b7913f74a1e1a5d64b3ede2b1c402466b134
royeradames <royeraadames@gmail.com>
2020-11-05 13:53:00 -0500

    test us_map 200 sucessful call

--- api/ds_server/ds_server.test.js
+++ api/ds_server/ds_server.test.js
@@ -7,9 +7,13 @@ const server = require('../app');
 const request = require('supertest');
 
 describe('ds_server', function () {
-  it('/ds_server 200', async () => {
-    const res = await request(server).get('/ds_server/unemploymentrate/FL');
+  it('/ds_server/us_map 200', async () => {
+    const res = await request(server).post('/ds_server/us_map').send({
+      start_date: '2013-01-01',
+      end_date: '2019-01-01',
+      sort_by: 'Demographic',
+    });
 
-    expect(res.status).toBe(201);
+    expect(res.status).toBe(200);
   });
 });
