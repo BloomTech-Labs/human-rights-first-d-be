40759d9c14710497bc4ed123d2a568fdcea5b251
royeradames <royeraadames@gmail.com>
2020-11-05 14:05:40 -0500

    fail test for us_map 404 no post body

--- api/ds_server/ds_server.test.js
+++ api/ds_server/ds_server.test.js
@@ -6,8 +6,8 @@ supertest
 const server = require('../app');
 const request = require('supertest');
 
-describe('ds_server', function () {
-  it('/ds_server/us_map 200', async () => {
+describe('/ds_server/', function () {
+  it('us_map 200', async () => {
     const res = await request(server).post('/ds_server/us_map').send({
       start_date: '2013-01-01',
       end_date: '2019-01-01',
@@ -15,5 +15,10 @@ describe('ds_server', function () {
     });
 
     expect(res.status).toBe(200);
+  });
+  it('us_map 404 missing post body', async function () {
+    const res = await request(server).post('/ds_server/us_map').send();
+
+    expect(res.status).toBe(404);
   });
 });
