8ef692ef70a0a2721daf2df095f67c8b78754b0e
royeradames <royeraadames@gmail.com>
2020-11-05 16:12:58 -0500

    add default inputs, get data from ds server, return ds data to client

--- api/ds_server/ds_server_router.js
+++ api/ds_server/ds_server_router.js
@@ -31,4 +31,39 @@ router.post('/us_map', async (req, res, next) => {
   }
 });
 
+router.post('/us_bar', async (req, res, next) => {
+  try {
+    /* 
+    Backend will handle the input validation
+    state have to be 2 letter and capitalize = GA FL CA
+    cities => no spaces like Atlanta,GA
+    //when you see first letter capitalize on DS doc that means that is how they want it
+    */
+
+    //set defaul values
+    const is_no_start_date = !req.body.start_date
+    const is_no_end_date = !req.body.end_date
+    const is_no_group_by = !req.body.group_by
+    const is_no_asc = !req.body.asc
+
+    if (is_no_start_date) req.body.start_date = "2013-01-01"
+    if (is_no_end_date) req.body.end_date = "2019-01-01"
+    if (is_no_group_by) req.body.group_by = {National: true}
+    if (is_no_asc) req.body.asc = true
+
+    
+    // get data from DS server
+    const us_bar = await axios.post(`http://hrf-ds16.eba-fmbjvhg4.us-east-1.elasticbeanstalk.com/us_bar`, {
+        start_date: req.body.start_date,
+        end_date: req.body.end_date,
+        group_by: req.body.group_by,
+        asc: req.body.asc
+      })
+
+    // send DS data to cliet  
+    res.status(200).json({ us_bar: us_bar.data })
+  } catch (error) {
+    next(error)
+  }
+})
 module.exports = router;
