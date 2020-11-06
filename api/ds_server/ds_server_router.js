a731fe0ab794e6060f4762fa130bf9b9792e3e8c
royeradames <royeraadames@gmail.com>
2020-11-06 16:19:43 -0500

    explain the endpoint code with comments. Remove zombie code

--- api/ds_server/ds_server_router.js
+++ api/ds_server/ds_server_router.js
@@ -13,6 +13,7 @@ router.post('/us_demo_pie', async (req, res, next) => {
       user_input: req.body.user_input
     })
 
+    //return DS server data to the client
     res.status(200).json({state_demographics: state_demographics.data})
   } catch (error) {
     next(error)
@@ -30,7 +31,7 @@ router.post('/us_map', async (req, res, next) => {
     if(is_no_sort_by) req.body.sort_by = "Demographic";
 
     //get data from ds_server
-    let a_state_unemployment_rate = await axios.post(
+    let incidents_rate = await axios.post(
       `http://hrf-ds16.eba-fmbjvhg4.us-east-1.elasticbeanstalk.com/us_map`,
       {
         start_date: req.body.start_date,
@@ -41,7 +42,7 @@ router.post('/us_map', async (req, res, next) => {
 
     //return send ds_server data
     res.status(200).json({
-      unemployment_rate: a_state_unemployment_rate.data,
+      incidents_rate: incidents_rate.data,
     });
   } catch (error) {
     next(error);
@@ -50,41 +51,6 @@ router.post('/us_map', async (req, res, next) => {
 
 router.post('/us_bar', async (req, res, next) => {
   try {
-    /* 
-    Backend will handle the input validation
-    state have to be 2 letter and capitalize = GA FL CA
-    cities => no spaces like Atlanta,GA
-    //when you see first letter capitalize on DS doc that means that is how they want it
-    */
-    //validate incoming inputs
-    //states inputs
-    // Todo: check that states var changes req...states
-    // const states = req.body.group_by.States
-    // const is_group_by_states = states 
-    // if(is_group_by_states){
-    //   states.forEach(state => {
-        
-    //     if (state.length === 2 ) {
-          
-    //       //change all given states abbreviation to uppercase
-    //       state.toUpperCase()
-    //     }
-    //     else{
-    //       //if the state input is not 2 characters ask user to change it to 2
-    //       res.status(404).json({error: "state abbreviation must be 2 letters"})
-    //     }
-        
-    //   })
-    // }
-
-    
-    //zipcodes
-    //no need, it will just not return data if put incorrect zipcode
-
-    //city no spaces like Atlanta,GA
-    // no spaces anywhere
-    // first letter capitalize
-    // state must be in upper case, and 2 chars
 
     //set defaul values
     const is_no_start_date = !req.body.start_date
