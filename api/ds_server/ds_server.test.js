def2d78f7fc76f8e24cf8af647445ffb6649a6eb
royeradames <royeraadames@gmail.com>
2020-11-13 11:09:16 -0500

    🔬sort_by Gender Armed.Unarmed is valid input

--- api/ds_server/ds_server.test.js
+++ api/ds_server/ds_server.test.js
@@ -1,6 +1,6 @@
 //imports
-const server = require('../app');
-const request = require('supertest');
+const server = require('../app')
+const request = require('supertest')
 
 
 describe('/us_demo_pie', () => {
@@ -29,41 +29,71 @@ describe('/us_demo_pie', () => {
     expect(res.status).toBe(404)
     expect(res.body.invalid_input).toEqual(expect.any(String))
   })
-  // it('404 input not')
 })
-describe('/us_map/', function () {
-  it('200 passing data to client', async () => {
+describe('/us_map/', () => {
+  it('200 validate inputs', async () => {
     const res = await request(server).post('/ds_server/us_map').send({
       start_date: '2013-01-01',
       end_date: '2019-01-01',
       sort_by: 'Demographic',
-    });
+    })
 
-    expect(res.status).toBe(200);
-  });
-  it('200 missing all of post input', async function () {
-    const res = await request(server).post('/ds_server/us_map').send();
+    expect(res.status).toBe(200)
+  })
+  it('200 default inputs works', async function () {
+    const res = await request(server).post('/ds_server/us_map').send()
 
-    expect(res.status).toBe(200);
-  });
-  it('200 missing part of post input', async function () {
+    expect(res.status).toBe(200)
+  })
+  it('200 sort_by Gender is valid', async () => {
     const res = await request(server).post('/ds_server/us_map').send({
-      start_date: '2013-01-01',
-      end_date: '2019-01-01',
-    });
+      sort_by: 'Gender'
+    })
 
-    expect(res.status).toBe(200);
-    expect(res.body).toEqual(expect.any(String));
-  });
-});
+    expect(res.status).toBe(200)
+    expect(res.body).toEqual(expect.any(String))
+  })
+  it('200 sort_by Armed/Unarmed is valid', async () => {
+    const res = await request(server).post('/ds_server/us_map').send({
+      sort_by: 'Armed/Unarmed'
+    })
+
+    expect(res.status).toBe(200)
+    expect(res.body).toEqual(expect.any(String))
+  })
+  it('404 incorrect start_date', async () => {
+    const res = await request(server).post('/ds_server/us_map').send({
+      start_date: '01-01-2013',
+    })
+
+    expect(res.status).toBe(404)
+    expect(res.body.invalid_input).toEqual(expect.any(String))
+  })
+  it('404 incorrect end_date', async () => {
+    const res = await request(server).post('/ds_server/us_map').send({
+      end_date: '01-01-2019'
+    })
+
+    expect(res.status).toBe(404)
+    expect(res.body.invalid_input).toEqual(expect.any(String))
+  })
+  it('404 invalid sort_by not one of the options', async () => {
+    const res = await request(server).post('/ds_server/us_map').send({
+      sort_by: 'not one of the valid options'
+    })
+
+    expect(res.status).toBe(404)
+    expect(res.body.invalid_input).toEqual(expect.any(String))
+  })
+})
 describe('/us_bar', () => {
   it('200 passing data to client', async () => {
     const res = await request(server).post(`/ds_server/us_bar`).send({
 
     })
 
-    expect(res.status).toBe(200);
-    expect(res.body).toEqual(expect.any(String));
+    expect(res.status).toBe(200)
+    expect(res.body).toEqual(expect.any(String))
   })
 })
 describe('/us_pie_vic', () => {
