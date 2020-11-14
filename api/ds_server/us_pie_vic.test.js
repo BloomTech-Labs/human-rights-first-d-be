a0b90c9d686f0f8ccf076c15f75afcb1dae3aeb8
royeradames <royeraadames@gmail.com>
2020-11-14 13:54:56 -0500

    🔬check: states and City options are working

--- api/ds_server/us_pie_vic.test.js
+++ api/ds_server/us_pie_vic.test.js
@@ -21,7 +21,7 @@ const url = '/ds_server/us_pie_vic'
 
   })
   describe('check all validation limits are working', () => {
-    it('',  async () => {
+    it('404 for start_date, end_date, and sort_by are invalid inputs',  async () => {
       const res = await request(server).post(url).send({
         start_date: "01-01-2013",
         end_date: "01-01-2019",
@@ -36,4 +36,31 @@ const url = '/ds_server/us_pie_vic'
       expect(errors).toMatch(/sort_by/i)
       expect(res.status).toBe(404)
     })
+
+    
+
   })
+describe('check all of group_by options are working', () => {
+  //National is already check by default values
+  it('200 states valid input', async () => {
+    const res = await request(server).post(url).send({
+      group_by: {States:["GA","FL","SC","CA","PA"]}
+
+    })
+    
+    expect(res.status).toBe(200)
+    expect(res.body).toMatch(/data/i)
+    expect(res.body).toMatch(/layout/i)
+  })
+
+  it('200 City valid inputs', async () => {
+      const res = await request(server).post(url).send({
+        group_by: { City: ["Atlanta, GA"]}
+      })
+      expect(res.body).toEqual(expect.any(String))
+      expect(res.body).toMatch(/data/i)
+      expect(res.body).toMatch(/layout/i)
+      expect(res.status).toBe(200)
+    })
+
+})
