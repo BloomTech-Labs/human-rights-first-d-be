cea740d9503e8396ea105ba8340cf177161602a1
royeradames <royeraadames@gmail.com>
2020-11-16 21:05:04 -0500

    👌remove ; onwrap axios data before saving on var

--- api/ds_server/ds_server_router.js
+++ api/ds_server/ds_server_router.js
@@ -1,6 +1,6 @@
 // libraries
-const axios = require('axios');
-const router = require('express').Router();
+const axios = require('axios')
+const router = require('express').Router()
 const {body, validationResult} = require('express-validator')
 
 const validate_us_demo_pie = [
@@ -49,21 +49,21 @@ router.post('/us_map', default_values_us_map, validate_us_map, async (req, res, 
     }
 
     //get data from ds_server
-    let incidents_rate = await axios.post(
+    let incidents_rate = (await axios.post(
       `http://hrf-ds16.eba-fmbjvhg4.us-east-1.elasticbeanstalk.com/us_map`,
       {
         start_date: req.body.start_date,
         end_date: req.body.end_date,
         sort_by: req.body.sort_by,
       }
-    );
+    )).data
 
     //return send ds_server data
-    res.status(200).json(incidents_rate.data);
+    res.status(200).json(incidents_rate)
   } catch (error) {
-    next(error);
+    next(error)
   }
-});
+})
 
 const validate_us_bar = [
   body('start_date').isDate(),
@@ -244,10 +244,10 @@ function default_values_us_map(req,res,next){
       const is_no_start_date = !req.body.start_date
       const is_no_end_date = !req.body.end_date
       const is_no_sort_by = !req.body.sort_by
-      if(is_no_start_date) req.body.start_date = "2013-01-01";
-      if(is_no_end_date) req.body.end_date = "2019-01-01";
-      if(is_no_sort_by) req.body.sort_by = "Demographic";
+      if(is_no_start_date) req.body.start_date = "2013-01-01"
+      if(is_no_end_date) req.body.end_date = "2019-01-01"
+      if(is_no_sort_by) req.body.sort_by = "Demographic"
       next()
     }
 
-module.exports = router;
+module.exports = router
